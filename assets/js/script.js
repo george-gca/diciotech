0253962ed319a8f346813f0b7009738bb439e280
George Araújo <george.gcac@gmail.com>
2024-10-03 22:40:15 -0300

    Changed favorite icons from png to svg
    Signed-off-by: George Araújo <george.gcac@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,9 @@ const cardsSection = document.querySelector("#cards");
 const filterSelect = document.querySelector("#tags-filter");
 let listOfCardsFiltered = [];
 let favoriteCards = [];
-const starIcon = "https://img.icons8.com/ios/50/star--v1.png";
-const starIconFilled =
-    "https://img.icons8.com/ios-glyphs/30/ffe100/star--v1.png";
+// SVG icons from https://heroicons.com/
+const starIcon = 'data:image/svg+xml;utf8,' + encodeURIComponent('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" /></svg>');
+const starIconFilled = 'data:image/svg+xml;utf8,' + encodeURIComponent('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6"><path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" /></svg>');
 
 function insertTagsIntoSelect(tags) {
     tags.sort();
@@ -178,11 +178,7 @@ function insertCardsIntoHtml(data) {
                     alt="star"
                     unique-title="${cardId}"
                     id="fav_${cardId}"
-                    src="${
-                        card.tags.includes("Favoritos")
-                            ? starIconFilled
-                            : starIcon
-                    }"
+                    src=${card.tags.includes("Favoritos") ? starIconFilled : starIcon}
                     class="fav__button"
                 />
             </div>
